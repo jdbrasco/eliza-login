8baf2febc17cec337425e8cd8b738113c3338064
jdbrasco <jdbrasco@gmail.com>
2024-03-12 16:39:26 -0400

    init checkin

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eliza_login_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
